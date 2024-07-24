51008888eb09a9bc427ec60f1c4d216630e3cd1d
denis.zubrin <dendel1998@gmail.com>
2024-07-24 11:01:21 +0300

    Initial build

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: false },
+  modules: ['nuxt-swiper', '@nuxt/eslint'],
+  // swiper: {
+  // Swiper options
+  //----------------------
+  // prefix: 'Swiper',
+  // styleLang: 'css',
+  // modules: ['navigation', 'pagination'], // all modules are imported by default
+  // }
+  // css: {
+
+  // }
+});
