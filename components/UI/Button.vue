cb1d9b5669da5004444a1c4e25780a4cb80d7019
denis.zubrin <dendel1998@gmail.com>
2024-07-29 17:14:19 +0300

    Prettier

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -13,63 +13,60 @@
 </template>
 
 <script setup>
-
-defineProps({
-  text: {
-    type: String,
-    required: true,
-  },
-  outline: Boolean
-});
-
+  defineProps({
+    text: {
+      type: String,
+      required: true,
+    },
+    outline: Boolean,
+  });
 </script>
 
 <style lang="scss">
+  .button {
+    border: none;
+    border-radius: 8px;
+    min-height: 53px;
+    box-sizing: border-box;
+    background-color: var(--color-text);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px 42px;
+    color: var(--color-filler);
+    position: relative;
+    cursor: pointer;
 
-.button {
-  border: none;
-  border-radius: 8px;
-  min-height: 53px;
-  box-sizing: border-box;
-  background-color: var(--color-text);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px 42px;
-  color: var(--color-filler);
-  position: relative;
-  cursor: pointer;
+    &__text {
+      display: block;
+      @extend %button_l;
+    }
 
-  &__text {
-    display: block;
-    @extend %button_l;
+    &__icon-wrap {
+      position: absolute;
+      top: calc(50% - 11px);
+      right: 8px;
+    }
+
+    &-icon {
+      fill: var(--color-filler);
+    }
+
+    &:hover &__icon-wrap &__icon {
+      fill: var(--color-text);
+    }
+
+    &:hover,
+    &:active {
+      background-color: var(--color-secondary);
+      color: var(--color-text);
+      border-color: var(--color-secondary);
+    }
+
+    &_outline {
+      background-color: transparent;
+      color: var(--color-text);
+      border: 1px solid var(--color-text);
+    }
   }
-
-  &__icon-wrap {
-    position: absolute;
-    top: calc(50% - 11px);
-    right: 8px;
-  }
-
-  &-icon {
-    fill: var(--color-filler);
-  }
-
-  &:hover &__icon-wrap &__icon {
-    fill: var(--color-text);
-  }
-
-  &:hover,
-  &:active {
-    background-color: var(--color-secondary);
-    color: var(--color-text);
-    border-color: var(--color-secondary);
-  }
-
-  &_outline {
-    background-color: transparent;
-    color: var(--color-text);
-    border: 1px solid var(--color-text);
-  }
-}
 </style>
